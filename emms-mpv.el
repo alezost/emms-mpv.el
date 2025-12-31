alezost/emms-mpv.el

commit 32155696fd1d6deb116408655dd00c06ac4161e6

Update track properly after metadada update
* emms-mpv.el (al/emms-mpv-update-current-track): New procedure.
  (emms-mpv-update-metadata): Use it instead of 'emms-track-updated'.

emms-mpv.el
@@ -712,7 +712,7 @@ instance is the global playlist."
       (ets info-genre       (key genre))
       (ets info-note        (key comment)))
     (when updated
-      (emms-track-updated track))))
+      (al/emms-mpv-update-current-track))))
 
 
 ;;; Hacks to make EMMS work with multiple players+playlists
@@ -744,6 +744,18 @@ This is similar to `emms-next-noerror', except it uses
     (emms-player-start (emms-playlist-selected-track)))
    (t
     (message "No next track in playlist"))))
+
+(defun al/emms-mpv-update-current-track ()
+  "Update `emms-playlist-selected-track' in the current playlist."
+  ;; We don't use `emms-track-updated' because it calls
+  ;; `emms-playlist-track-updated' which checks all(!) emacs buffers and
+  ;; updates the track in all playlists.
+  (when emms-playlist-selected-marker
+    (save-excursion
+      (goto-char emms-playlist-selected-marker)
+      (emms-playlist-update-track))
+    (emms-mpv-update-global-state-maybe
+     (current-buffer) 'track)))
 
 (defun emms-mpv-update-global-state-maybe (buffer &rest keywords)
   "Update global state if BUFFER is the current EMMS playlist."

commit 4954b5d8c6c55e93e420c1e6768f839ccc782a7e

Fix typo
Commit 32155696fd1d6deb116408655dd00c06ac4161e6 accidentally introduced
'al/emms-mpv-update-current-track' instead of 'emms-mpv-update-current-track'.

emms-mpv.el
@@ -678,7 +678,7 @@ instance is the global playlist."
                             (emms-track-name track))
         (setq duration (round duration))
         (emms-track-set track 'info-playing-time duration)
-        (al/emms-mpv-update-current-track)))))
+        (emms-mpv-update-current-track)))))
 
 (defun emms-mpv-handle-property-metadata (json-data)
   "Handler for \"metadata\" property change."
@@ -711,7 +711,7 @@ instance is the global playlist."
       (ets info-genre       (key genre))
       (ets info-note        (key comment)))
     (when updated
-      (al/emms-mpv-update-current-track))))
+      (emms-mpv-update-current-track))))
 
 
 ;;; Hacks to make EMMS work with multiple players+playlists
@@ -744,7 +744,7 @@ This is similar to `emms-next-noerror', except it uses
    (t
     (message "No next track in playlist"))))
 
-(defun al/emms-mpv-update-current-track ()
+(defun emms-mpv-update-current-track ()
   "Update `emms-playlist-selected-track' in the current playlist."
   ;; We don't use `emms-track-updated' because it calls
   ;; `emms-playlist-track-updated' which checks all(!) emacs buffers and