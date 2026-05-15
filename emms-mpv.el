alezost/emms-mpv.el

commit f05d17fb38c4b48fb8c25197a911de9bc3893cec

Remove progress for almost finished files
* emms-mpv.el (emms-mpv-progress-check-time): Remove progress if
`emms-mpv-progress-threshold' seconds or less left till the end of track.

emms-mpv.el
@@ -1097,9 +1097,13 @@ Return nil, if there is no NAME key `emms-mpv-progress-data'."
 See `emms-mpv-progress-threshold' for details."
   (not
    (or (< progress emms-mpv-progress-threshold)
-       (let* ((total (emms-track-get track 'info-playing-time))
-              (left  (- total progress)))
-         (< left emms-mpv-progress-threshold)))))
+       (when-let* ((total (emms-track-get track 'info-playing-time)))
+         (let* ((left  (- total progress))
+                (savep (< left emms-mpv-progress-threshold)))
+           (and emms-mpv-progress-remove-finished
+                (not savep)
+                (emms-mpv-progress-remove (emms-track-name track)))
+           savep)))))
 
 (defun emms-mpv-progress-initialize-maybe ()
   "Initialize `emms-mpv-progress-data' if not yet initialized.

commit 113c02eb58cc30c0bd5d0a847f00082fff3bd842

Fix removing progress for almost finished files
This bug was introduced by commit f05d17fb38c4b48fb8c25197a911de9bc3893cec.

Before this commit, progress for almost finished files was never removed.
Instead, the usual progress (of not almost finished files) was removed.
This was not noticed earlier because this removed progress was saved again
right after that by `emms-mpv-save-track-progress-maybe'.

emms-mpv.el
@@ -1128,11 +1128,11 @@ See `emms-mpv-progress-threshold' for details."
    (or (< progress emms-mpv-progress-threshold)
        (when-let* ((total (emms-track-get track 'info-playing-time)))
          (let* ((left  (- total progress))
-                (savep (< left emms-mpv-progress-threshold)))
+                (skipp (< left emms-mpv-progress-threshold)))
            (and emms-mpv-progress-remove-finished
-                (not savep)
+                skipp
                 (emms-mpv-progress-remove (emms-track-name track)))
-           savep)))))
+           skipp)))))
 
 (defun emms-mpv-progress-check-small-track (track _progress)
   "Return nil if TRACK is too small for saving.